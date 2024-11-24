ca9ce077518eb773ec000ab7bc0a11728ed405c4
ismael8989 <ismaelirden@gmail.com>
2024-11-24 13:32:06 +0100

    add reset functionality, fix responsivity issues

--- script.js
+++ script.js
@@ -1,52 +1,89 @@
-const render_seconds = function(seconds) {
-    if (typeof seconds !== 'number' || seconds < 0) {
-        return 'Invalid input';
-    }
-
-    let minutes = Math.floor(seconds / 60);
-    minutes = minutes.toString().padStart(2, '0');
-    let remaining_seconds = seconds % 60;
-    remaining_seconds = remaining_seconds.toString().padStart(2, '0');
-
-    return `${minutes}<span>min</span> ${remaining_seconds}<span>sec</span>`;
-};
-
+import { render_seconds } from "./useful_functions.js";
 // html elements
 const time = document.querySelector("#timer h1");
 const button = document.querySelector("button");
 const timer_container = document.querySelector("#timer");
+const reset_hint = document.querySelector("p.reset-hint");
+
+// import sounds
 const sound = new Audio("./resources/sound.mp3");
+const reset_sound = new Audio("./resources/reset-sound.mp3");
 
 // general variables
-let focus_time = 25;
-let break_time = 5;
+const focus_time = 25;
+const break_time = 5;
 let countdown_time = focus_time;
+let is_timerOn = false;
 
 // display time on title
 time.innerHTML = render_seconds(countdown_time);
 
+// reset UI after timer has ended or been stopped
+const reset_timer = function(){
+    is_timerOn = false;
+
+    button.innerText = "start";
+    button.disabled = false;
+    button.style.opacity = "1";
+    button.style.cursor = "pointer";
+
+    document.body.style.backgroundColor = "white";
+
+    timer_container.style.boxShadow = "none";
+
+    reset_hint.style.opacity = "0";
+    reset_hint.style.transform = "translateY(100px)";
+
+}
+
 // clicking the button logic
 const countdown = function(seconds) {
-    time.innerHTML = render_seconds(seconds);
+    seconds = Math.max(0 , seconds);
+    time.innerHTML = render_seconds(Math.ceil(seconds));
     if (seconds == 0) {
-        button.innerText = "start";
-        button.disabled = false;
-        button.style.cursor = "pointer";
-        document.body.style.backgroundColor = "white";
-        timer_container.style.boxShadow = "none";
+
+        reset_timer();
+
         sound.play();
+
         countdown_time = countdown_time == focus_time ? break_time : focus_time;
         time.innerHTML = render_seconds(countdown_time);
+
         return;
     }
-    setTimeout(() => countdown(seconds - 1), 1000);
+
+    if(!is_timerOn){
+
+        reset_timer();
+
+        reset_sound.play();
+        time.innerHTML = render_seconds(countdown_time);
+
+        return;
+    }
+
+    setTimeout(() => countdown(seconds - .1), 100);
 };
 
 button.addEventListener("click", () => {
+  is_timerOn = true;
   countdown(countdown_time);
-  button.textContent = "FOCUS";
+
+  button.textContent = countdown_time == focus_time ? "FOCUS" : "REST";
   button.disabled = true;
+  button.style.opacity = ".5";
   button.style.cursor = "default";
+
   timer_container.style.boxShadow = "0 0 2rem coral";
+
   document.body.style.backgroundColor = "#222";
+
+  reset_hint.style.opacity = "1";
+  reset_hint.style.transform = "translateY(0)";
 });
+
+window.addEventListener("keypress", (e) => {
+    if(e.key === " "){
+        is_timerOn = false;
+    }
+})
